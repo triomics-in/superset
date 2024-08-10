3bd7b725b95acfe3267a051dd4b22303d3b1c320
siddthanthchopra <140395257+siddthanthchopra@users.noreply.github.com>
2024-08-10 14:54:29 +0530

    Update Menu.tsx

--- superset-frontend/src/features/home/Menu.tsx
+++ superset-frontend/src/features/home/Menu.tsx
@@ -311,11 +311,11 @@ export function Menu({
                 to="/nurse/case-finding"
                 tabIndex={-1}
               >
-                <img src="../../assets/images/TriomicsLogoHorizon.png" alt="Triomics" />
+                <img src="../../assets/images/TriomicsLogoHorizon.png"/>
               </GenericLink>
             ) : (
               <a className="navbar-brand" href={brand.path} tabIndex={-1}>
-                <img src="../../assets/images/TriomicsLogoHorizon.png" alt="Triomics" />
+                <img src="../../assets/images/TriomicsLogoHorizon.png"/>
               </a>
             )}
           </Tooltip>
